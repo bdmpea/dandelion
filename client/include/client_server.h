874e47f1579da00717575fb231c1e571a00d4cd6
GrigoriyNovitskiy <112243858+GrigoriyNovitskiy@users.noreply.github.com>
2023-03-06 12:45:58 +0300

    Add files via upload

---
+++ client/include/client_server.h
@@ -0,0 +1,14 @@
+#ifndef UNTITLED2_CLIENT_SERVER_H
+#define UNTITLED2_CLIENT_SERVER_H
+namespace Dandelion::Client {
+    struct client_server{
+
+    };
+    struct get_request : client_server{
+
+    };
+    struct parse_request : client_server{
+
+    };
+}
+#endif //UNTITLED2_CLIENT_SERVER_H
