7ef4c02d93ef3748e0a0739b6be882c7d30aaf5b
GrigoriyNovitskiy <112243858+GrigoriyNovitskiy@users.noreply.github.com>
2023-03-15 21:35:23 +0300

    Update server.hpp

--- server/include/server.hpp
+++ server/include/server.hpp
@@ -1,10 +1,19 @@
 #ifndef UNTITLED2_SERVER_HPP
 #define UNTITLED2_SERVER_HPP
+#include <iostream>
+#include "string"
 namespace Dandelion::Server {
-    struct server {
+    struct server{
+    public:
+        bool register_user(const std::string &login,const std::string &password);
+        bool check_signing_in(const std::string &login,const std::string &password);
+        static server &getInstance(){
+            static server Server;
+            return Server;
+        }
 
     };
-    struct get_client{
+    struct model_changes{
 
     };
 }
