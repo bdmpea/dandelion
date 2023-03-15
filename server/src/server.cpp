4812ce53d1e5fdd6c936a9c4471db600a98717d7
GrigoriyNovitskiy <112243858+GrigoriyNovitskiy@users.noreply.github.com>
2023-03-15 21:34:52 +0300

    Update server.cpp

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,1 +1,20 @@
 #include "server.hpp"
+namespace Dandelion::Server{
+    bool server::register_user(const std::string &login, const std::string &password){
+        try {
+          //  is_used_login(login);    use function hear to connect with BD
+        }catch(std::exception &exp){
+            return false;
+        }
+        //create_user(login, password);
+        return true;
+    }
+    bool server::check_signing_in(const std::string &login,const std::string &password){
+        try {
+          //  is_correct_account_login(login, password);   use function hear to connect with BD
+        }catch(std::exception &exp){
+            return false;
+        }
+        return true;
+    }
+}
