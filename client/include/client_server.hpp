4b5ef078b53b63cf86c8c15c4b435b30e5cf8946
GrigoriyNovitskiy <112243858+GrigoriyNovitskiy@users.noreply.github.com>
2023-04-15 18:55:30 +0300

    Update client_server.hpp

--- client/include/client_server.hpp
+++ client/include/client_server.hpp
@@ -1,14 +1,30 @@
-#ifndef UNTITLED2_CLIENT_SERVER_HPP
-#define UNTITLED2_CLIENT_SERVER_HPP
-namespace Dandelion::Client {
-    struct client_server{
+#ifndef CLIENT_SERVER_H
+#define CLIENT_SERVER_H
 
-    };
-    struct get_request : client_server{
 
-    };
-    struct parse_request : client_server{
+#include <QWidget>
+#include <QTcpSocket>
 
-    };
-}
-#endif //UNTITLED2_CLIENT_SERVER_HPP
+class QTextEdit;
+class QLineEdit;
+
+class client_server : public QWidget {
+Q_OBJECT
+private:
+    QTcpSocket* m_pTcpSocket;
+    quint16     m_nNextBlockSize;
+
+public:
+    client_server(const QString& ip, int nPort, QWidget* pwgt = 0) : QWidget(pwgt), m_nNextBlockSize(0){
+        m_pTcpSocket = new QTcpSocket(this);
+        m_pTcpSocket->connectToHost(ip, nPort);
+        connect(m_pTcpSocket, SIGNAL(connected()), SLOT(slotConnected()));
+        connect(m_pTcpSocket, SIGNAL(readyRead()), SLOT(slotReadyRead()));
+    }
+
+private slots:
+   void slotReadyRead();
+   void slotSendToServer();
+};
+
+#endif // CLIENT_SERVER_H
