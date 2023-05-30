65c1c5d19c704ce1adeefba376a0892c0b0e758e
karambo3a <112202183+karambo3a@users.noreply.github.com>
2023-05-30 12:30:29 +0300

    upload flashcards .h files

---
+++ client/include/welcome.h
@@ -0,0 +1,28 @@
+#ifndef WELCOME_H
+#define WELCOME_H
+
+#include <QWidget>
+#include <QMainWindow>
+#include "main_menu.h"
+
+namespace Ui {
+class Welcome;
+}
+
+class Welcome : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Welcome(QMainWindow *parent = nullptr);
+    ~Welcome();
+
+private slots:
+    void on_ContinueButton_clicked();
+
+private:
+    Ui::Welcome *ui;
+    Main_Menu *main_window;
+};
+
+#endif // WELCOME_H
