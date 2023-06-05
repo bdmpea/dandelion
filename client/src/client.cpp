8441aa034b8b807eb2ea64ac84985d9262a9e1d9
GrigoriyNovitskiy <112243858+GrigoriyNovitskiy@users.noreply.github.com>
2023-06-05 12:18:28 +0300

    Add files via upload

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,48 +1,63 @@
 #include <utility>
+#include <QDataStream>
 
-#include "../../server/include/client.h"
+#include "../include/client.h"
 
 client::client(QObject *parent) {
-//    m_main_window = new MainWindow();
-//    connect(m_main_window, SIGNAL(signal_info_for_authorization()), this, SLOT(got_info_for_authorization()));
-//    connect(m_main_window, SIGNAL(signal_info_for_registrtion()), this,
-//            SLOT(got_info_for_registrtion()));
+    m_main_window = new MainWindow();
+    connect(m_main_window, SIGNAL(signal_info_for_authorization()), this,
+            SLOT(got_info_for_authorization()));
+    connect(m_main_window, SIGNAL(signal_info_for_registration()), this,
+            SLOT(got_info_for_registration()));
 }
-
 
 
 client::~client() noexcept {
-  //  m_main_window->deleteLater();
+    m_main_window->deleteLater();
 }
 
 void client::start() {
-   // m_main_window->show();
+    m_main_window->show();
     m_client_socket = new socket_on_client(1338, "127.0.0.1");
-    m_client_socket->connect();
+    m_client_socket->connect_();
+    connect(m_client_socket, SIGNAL(signal_info_for_registration_status(User_information)), this, SLOT(info_for_registration_status(User_information)));
+    connect(m_client_socket, SIGNAL(signal_info_for_authorization_status(User_information)), this,
+            SLOT(info_for_authorization_status(User_information)));
 }
 
 void client::got_info_for_registration() {
-
+    qDebug() << "Got register data";
+    m_client_socket->try_register_user(m_main_window->registration->get_username(),
+                                       m_main_window->registration->get_password());
 }
 
 void client::got_info_for_authorization() {
-
+    m_client_socket->try_sign_in_user(m_main_window->get_username(), m_main_window->get_password());
 }
 
+void client::info_for_authorization_status(User_information user) {
+    m_user = std::move(m_client_socket->m_user);
+    m_main_window->authorize(m_user.m_status);
+}
+
+void client::info_for_registration_status(User_information user) {
+    m_user = std::move(m_client_socket->m_user);
+    m_main_window->registration->register_user(m_user.m_status);
+}
 
 socket_on_client::socket_on_client(qint16 port, QString ip) : m_client_socket(new QTcpSocket(this)), m_PORT(port),
                                                               m_ip(std::move(ip)),
-                                                              m_user("", "") {};
+                                                              m_user("", "", "") {};
 
 void socket_on_client::send_data_to_server(const QByteArray &data) {
     m_client_socket->write(data);
     m_client_socket->waitForBytesWritten();
 }
 
-void socket_on_client::connect() {
+void socket_on_client::connect_() {
     m_client_socket->connectToHost(m_ip, m_PORT);
     if (m_client_socket->waitForConnected()) {
-        //TODO: error
+        connect(m_client_socket, SIGNAL(readyRead()), this, SLOT(validate_server_answer()));
     }
 }
 
@@ -50,47 +65,69 @@ socket_on_client::~socket_on_client() {
     m_client_socket->deleteLater();
 }
 
-bool socket_on_client::sign_in_user(const QString &username, const QString &password) {
+void socket_on_client::validate_server_answer() {
+    QDataStream in(&(*m_client_socket));
+    quint16 block_size;
+    in >> block_size;
+    m_client_socket->read(sizeof(quint32));
+    QByteArray server_response = m_client_socket->read(block_size);
+    QJsonParseError error{};
+    QJsonDocument answer = QJsonDocument::fromJson(server_response, &error);
+    if (error.errorString().toInt() == QJsonParseError::NoError) {
+        QString type = answer.object().value("type").toString();
+        if (type == "sign_in") {
+            check_sign_in(answer);
+            emit signal_info_for_authorization_status(m_user);
+        } else if (type == "registration") {
+            check_register_user(answer);
+            emit signal_info_for_registration_status(m_user);
+        }
+    } else {
+        qDebug() << "Some errors with server data";
+    }
+}
+
+void socket_on_client::try_sign_in_user(const QString &username, const QString &password) {
     QJsonObject request;
     request["type"] = "signing_in";
     request["username"] = username;
     request["password"] = password;
     QJsonDocument m_request(request);
     send_data_to_server(m_request.toJson());
-    QDataStream in(&(*m_client_socket));
-    quint16 block_size;
-    in >> block_size;
-    m_client_socket->read(sizeof(quint32));
-    QByteArray server_response = m_client_socket->read(block_size);
-   // QByteArray server_response = m_client_socket->readAll();
-    QJsonParseError error{};
-    QJsonDocument answer = QJsonDocument::fromJson(server_response, &error);
-    qDebug() << answer.object().value("password").toString();
-    if (answer.object().value("status").toString() == "success") {
-        m_user = User_information(answer.object().value("username").toString(),
-                                  answer.object().value("password").toString());
-        return true;
-    } else {
-        return false;
-    }
 }
 
-bool socket_on_client::register_user(const QString &username, const QString &password) {
+void socket_on_client::try_register_user(const QString &username, const QString &password) {
+    qDebug() << "Try register";
     QJsonObject request;
     request["type"] = "registration";
     request["username"] = username;
     request["password"] = password;
     QJsonDocument m_request(request);
     send_data_to_server(m_request.toJson());
-    m_client_socket->waitForReadyRead();
-    QByteArray server_response = m_client_socket->readAll();
-    QJsonParseError error;
-    QJsonDocument answer = QJsonDocument::fromJson(server_response, &error);
+}
+
+void socket_on_client::check_sign_in(const QJsonDocument &answer) {
+    qDebug() << "After sending" << answer.object().value("status").toString();
+    qDebug() << "After sending" << answer.object().value("password").toString();
+    qDebug() << "After sending" << answer.object().value("username").toString();
     if (answer.object().value("status").toString() == "success") {
         m_user = User_information(answer.object().value("username").toString(),
-                                  answer.object().value("password").toString());
-        return true;
+                                  answer.object().value("password").toString(),
+                                  answer.object().value("status").toString());
+        qDebug() << "user" << answer.object().value("status").toString();
+        qDebug() << "user" << answer.object().value("password").toString();
+        qDebug() << "user" << answer.object().value("username").toString();
     } else {
-        return false;
+        m_user = User_information("", "", "error");
+    }
+}
+
+void socket_on_client::check_register_user(const QJsonDocument &answer) {
+    if (answer.object().value("status").toString() == "success") {
+        m_user = User_information(answer.object().value("username").toString(),
+                                  answer.object().value("password").toString(),
+                                  answer.object().value("status").toString());
+    } else {
+        m_user = User_information("", "", "error");
     }
 }
---
+++ client/src/main.cpp
@@ -0,0 +1,18 @@
+#include "../include/main_window.h"
+#include "../../server/include/server.hpp"
+#include "../include/client.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR","1");
+    QApplication a(argc, argv);
+    qint16 port = 1338;
+    std::string connection_string = "host=127.0.0.1 port=5432 dbname=p34096 user=postgres password = Grisha09012004";
+    if(!Dandelion::Server::server::getInstance().start_server(port, connection_string)) {
+        return 0;
+    }
+    client first_client;
+    first_client.start();
+    return a.exec();
+}
