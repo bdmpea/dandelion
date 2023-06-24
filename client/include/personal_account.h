0aad84d7633fe52568f17d0fc481ba798d49f7e3
karambo3a <112202183+karambo3a@users.noreply.github.com>
2023-06-24 23:52:47 +0300

    update personal_account.h

--- client/include/personal_account.h
+++ client/include/personal_account.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include "info_window.h"
 #include "flashcards_desk.h"
+#include "vocabulary_list.h"
 
 
 
@@ -19,21 +20,24 @@ public:
     explicit PersonalAccount(QWidget *parent = nullptr);
     ~PersonalAccount();
 
+    QString get_new_word();
+
 
 private slots:
     void on_BackButton_clicked();
     void on_VocabularyButton_clicked();
     void on_FlashcardsButton_clicked();
     void close_window();
+    void open_window();
 
     void on_NewWordButton_clicked();
-    QString get_new_word();
     void on_AddButton_2_clicked();
 
 private:
     Ui::PersonalAccount *ui;
     info_window *info;
     flashcards_desk *desk;
+    vocabulary_list *vocabulary;
 };
 
 #endif // PERSONAL_ACCOUNT_H
