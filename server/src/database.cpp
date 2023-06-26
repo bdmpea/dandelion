4c392ac4ff93902ceef8759c8eae8c3f407c0861
GrigoriyNovitskiy <112243858+GrigoriyNovitskiy@users.noreply.github.com>
2023-06-26 22:40:35 +0300

    Initialize server

---
+++ server/src/database.cpp
@@ -0,0 +1,122 @@
+#include "../include/database.hpp"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QObject>
+#include <QTcpSocket>
+#include <QVector>
+
+namespace Dandelion::Server {
+    Database::Database(const std::string &connection_string) : m_connection(connection_string.c_str()) {}
+
+    unsigned int User_Database::is_used_login(const std::string &login, Database &db) {
+        pqxx::work worker(db.m_connection);
+        int user_id;
+        try {
+            user_id = worker.query_value<int>("SELECT id from register_users WHERE username='" + login + "'");
+            //TODO
+        }
+        catch (...) {
+            return 0;
+        }
+        return user_id;
+    }
+
+    User User_Database::create_user(const std::string &login, const std::string &password, Database &db) {
+        try {
+            if (!is_used_login(login, db)) {
+                db.Database::do_query("INSERT INTO register_users"\
+            "(username, password)"\
+            "VALUES(\'" + login + "\',\'" + password + "\');"
+                );
+                return {login, password};
+            }
+        } catch (...) {
+            return {"", ""};
+        }
+        return {"", ""};
+    }
+
+    void Database::do_query(const std::string &query) {
+        pqxx::work worker(m_connection);
+        worker.exec(query);
+        worker.commit();
+    }
+
+    User User_Database::make_signing_in(const std::string &login, const std::string &password, Database &db) {
+        unsigned int user_id = is_used_login(login, db);
+          qDebug() << user_id;
+        if (user_id) {
+            pqxx::work worker(db.m_connection);
+            auto correct_password = worker.query_value<std::string>(
+                    "SELECT password FROM register_users WHERE username ='" + login + "\'");
+
+            if (password == correct_password) {
+                create_personal_dictionary(worker, login);
+                return {login, password};
+            } else {
+                return {"", ""};
+            }
+        }
+        return {"", ""};
+    }
+
+    bool User_Database::add_new_word(const std::string &login, const std::string &word, Database &db) {
+        try {
+            db.Database::do_query("INSERT INTO " + login + "(word, meaning, examples) VALUES"\
+            "(\'" + word + "\' , 'some meaning', 'some example')"
+            );
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    void User_Database::create_personal_dictionary(pqxx::work &worker, const std::string &login) {
+        try {
+            worker.exec("CREATE TABLE IF NOT EXISTS " + login + " ( "\
+         " id serial  PRIMARY KEY,"\
+         " word character varying(32) NOT NULL,"\
+         " meaning character varying(64) NOT NULL,"\
+         " examples  character varying(64) NOT NULL );"
+            );
+
+            bool if_empty = worker.query_value<bool>(" SELECT(SELECT count(*) FROM " + login + ") = 0 ");
+            qDebug() << if_empty;
+            if (if_empty) {
+                worker.exec("INSERT INTO " + login + "(word, meaning, examples) VALUES"\
+            "('cat', 'a small  carnivorous mammal with soft fur', 'black cat'),"\
+            "('dog', 'a  carnivorous mammal that  has a long snout', 'white dog'),"\
+            "('commit', 'perform an act with a negative connotation', 'make new commit'),"\
+            "('issue', 'some situation or event that is thought about', 'hard issue'),"\
+            "('stock', 'capital raised by a corporation ', 'apple stock is cheap'),"\
+            "('circumstances', 'one overall condition in life', 'the circumstances leading up'),"\
+            "('level', 'a relative position or degree of value ', 'rates reach the same level'),"\
+            "('affect', 'have an influence upon', 'companies affected by the flooding'),"\
+            "('institute', 'set up or lay the groundwork for', 'focused on instituting higher labor'),"\
+            "('render', 'give an interpretation of', 'but authorities had rendered the weapon')"
+                );
+            }
+            worker.commit();
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    QVector<QVector<QString>> User_Database::get_vocabulary(const std::string &login, Database &db) {
+        QVector<QVector<QString>> vocabulary;
+        try{
+            pqxx::work worker(db.m_connection);
+            pqxx::result result = worker.exec("SELECT * FROM " + login);
+            vocabulary.resize(result.size());
+            for(int i = 0; i < result.size(); i++){
+                vocabulary[i].push_back(QString::fromStdString(to_string(result[i][1])));
+                vocabulary[i].push_back(QString::fromStdString(to_string(result[i][2])));
+            }
+        } catch(const std::exception &e){
+            std::cerr << e.what() << std::endl;
+        }
+        return vocabulary;
+    }
+}
+
--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,1 +1,230 @@
-#include "server.hpp"
+#include "../include/server.hpp"
+#include <QTcpServer>
+#include <QTcpSocket>
+
+namespace Dandelion::Server {
+
+    server::server(QObject *parent) {
+
+    }
+
+    bool server::start_server(qint16 port, std::string &connection_string) {
+        m_Server = new QTcpServer(this);
+        PORT = port;
+
+        if (m_Server->listen(QHostAddress::Any, PORT)) {
+            qDebug() << "Listening port" << PORT << "...";
+            qDebug() << "My ip " << m_Server->serverAddress().toString();
+
+            connect(
+                    m_Server, SIGNAL(newConnection()), this, SLOT(incomingConnection())
+            );
+
+            if (connect_to_database(connection_string)) {
+                qDebug() << "Connected to database";
+            } else {
+                qDebug() << "Couldn't connect to database";
+                return false;
+            }
+        } else {
+            qDebug() << "Couldn't start server";
+            return false;
+        }
+        return true;
+    }
+
+    void server::disconnect_socket(client_socket *socket) {
+        m_sockets.erase(m_sockets.find((socket->get_socket_id())));
+        delete socket;
+    }
+
+    bool server::connect_to_database(std::string &connection_string) {
+        static Database db(connection_string);
+        m_db = &db;
+        return db.m_connection.is_open();
+    }
+
+    void server::incomingConnection() {
+        qDebug() << "New connection";
+        QTcpSocket *new_connection = m_Server->nextPendingConnection();
+        auto *new_socket = new client_socket(new_connection, m_db, this, new_connection->socketDescriptor());
+
+        m_sockets[new_connection->socketDescriptor()] = new_socket;
+    }
+
+    server::~server() {
+        for (auto socket: m_sockets) {
+            socket->m_socket->deleteLater();
+        }
+
+        m_Server->deleteLater();
+     //   delete m_db;
+    }
+
+
+
+
+
+    client_socket::client_socket(QTcpSocket *socket, Database *db, server *server, quint16 id, QObject *parent)
+            : m_socket(socket), m_db(db), m_server(server), m_socket_id(id),
+              QObject(nullptr) {
+        connect(m_socket, SIGNAL(disconnected()),
+                this, SLOT(disconnect())
+        );
+        connect(m_socket, SIGNAL(readyRead()),
+                this, SLOT(read_from_client())
+        );
+        m_request = new client_request(m_db);
+    }
+
+
+    client_socket::~client_socket() {
+        m_socket->deleteLater();
+    }
+
+    void client_socket::send_to_client(const QByteArray &data) {
+        QByteArray block;
+        QDataStream out(&block, QIODevice::WriteOnly);
+        out << (quint16) data.size();
+        out << data;
+        m_socket->write(block);
+        // m_socket->flush();
+        m_socket->waitForBytesWritten();
+    }
+
+    void client_socket::read_from_client() {
+        QByteArray data = m_socket->readAll();
+        qDebug() << "got from client";
+        QJsonDocument response = m_request->validate_request(data);
+        qDebug() << "Before sending" << response.object().value("status").toString();
+        qDebug() << "Before sending" << response.object().value("username").toString();
+        qDebug() << "Before sending" << response.object().value("type").toString();
+        qDebug() << "Before sending" << response.object().value("word1").toString();
+        qDebug() << "Before sending" << response.object().value("meaning1").toString();
+        send_to_client(response.toJson());
+    }
+
+    int client_socket::get_socket_id() {
+        return m_socket_id;
+    }
+
+    void client_socket::disconnect() {
+        m_server->disconnect_socket(this);
+    }
+
+
+
+    client_request::client_request(Database *db) : m_db(db) {
+    }
+
+    QJsonDocument client_request::validate_request(const QByteArray &data) {
+        QJsonParseError json_data_error{};
+        QJsonDocument json_data = QJsonDocument::fromJson(data, &json_data_error);
+        if (json_data_error.errorString().toInt() == QJsonParseError::NoError) {
+            QString request_type = json_data.object().value("type").toString();
+            if (!request_type.size()) {
+                QJsonObject json_response;
+                json_response["status"] = "error";
+                json_response["text"] = "missing request type";
+                QJsonDocument response(json_response);
+                return response;
+            }
+            if (request_type == "signing_in") {
+                QJsonDocument answer = sign_in(json_data.object());
+                QJsonObject obj = answer.object();
+                obj["type"] = "sign_in";
+                answer.setObject(obj);
+                return answer;
+            } else if (request_type == "registration") {
+                QJsonDocument answer = make_registration(json_data.object());
+                QJsonObject obj = answer.object();
+                obj["type"] = "registration";
+                answer.setObject(obj);
+                return answer;
+            } else if (request_type == "new_word") {
+                QJsonDocument answer = add_new_word(json_data.object());
+                QJsonObject obj = answer.object();
+                obj["type"] = "new_word";
+                answer.setObject(obj);
+                return answer;
+            }else if (request_type == "get_vocabulary"){
+                QJsonDocument answer = get_words_from_vocabulary(json_data.object());
+                QJsonObject obj = answer.object();
+                obj["type"] = "get_vocabulary";
+                answer.setObject(obj);
+                return answer;
+            } else {
+                QJsonObject json_response;
+                json_response["status"] = "error";
+                json_response["text"] = "invalid request type";
+                QJsonDocument response(json_response);
+                return response;
+            }
+        }
+        return json_data;
+    }
+
+
+    QJsonDocument client_request::sign_in(const QJsonObject &request) {
+        QString received_login = request.value("username").toString();
+        QString received_password = request.value("password").toString();
+        User is_correct_user = User_Database::make_signing_in(received_login.toStdString(),
+                                                              received_password.toStdString(), *m_db);
+        return check_request(is_correct_user, "failed authorization");
+    }
+
+
+    QJsonDocument client_request::check_request(User user, const std::string &error_text) {
+        QJsonObject response;
+        if (user.m_username.empty() || user.m_password.empty()) {
+            response["status"] = "error";
+            response["text"] = QString::fromStdString(error_text);
+            QJsonDocument server_response(response);
+            return server_response;
+        }
+        response["status"] = "success";
+        response["username"] = QString::fromStdString(user.m_username);
+        response["password"] = QString::fromStdString(user.m_password);
+        QJsonDocument server_response(response);
+        return server_response;
+    }
+
+    QJsonDocument client_request::make_registration(const QJsonObject &request) {
+        QString received_login = request.value("username").toString();
+        QString received_password = request.value("password").toString();
+        User probably_new_user = User_Database::create_user(received_login.toStdString(),
+                                                            received_password.toStdString(), *m_db);
+        return check_request(probably_new_user, "failed registration");
+    }
+
+    QJsonDocument client_request::add_new_word(const QJsonObject &request) {
+        QJsonObject response;
+        QString received_login = request.value("username").toString();
+        QString received_word = request.value("word").toString();
+        bool result = User_Database::add_new_word(received_login.toStdString(), received_word.toStdString(), *m_db);
+        if (result) {
+            response["status"] = "success";
+            QJsonDocument server_response(response);
+            return server_response;
+        }
+        response["status"] = "error";
+        QJsonDocument server_response(response);
+        return server_response;
+    }
+    QJsonDocument client_request::get_words_from_vocabulary(const QJsonObject &request) {
+        QJsonObject response;
+        QString received_login = request.value("username").toString();
+        QVector<QVector<QString>> vocabulary = User_Database::get_vocabulary(received_login.toStdString(), *m_db);
+        std::string size = std::to_string(vocabulary.size());
+        response["size"] = QString::fromStdString(size);
+        response["status"] = "success";
+        int i = 0;
+        for(auto word: vocabulary){
+            response["word" + QString::fromStdString(std::to_string(i))] = word[0];
+            response["meaning" + QString::fromStdString(std::to_string(i))] = word[1];
+            i++;
+        }
+        QJsonDocument server_response(response);
+        return server_response;
+    }
+}
