9b558892deb85bb3a19d445f2fc3048c8815c3a2
GrigoriyNovitskiy <112243858+GrigoriyNovitskiy@users.noreply.github.com>
2023-06-26 22:42:36 +0300

    Add files via upload

--- server/include/database.hpp
+++ server/include/database.hpp
@@ -1,25 +1,52 @@
 #ifndef DANDELION_DATABASE_H
 #define DANDELION_DATABASE_H
 
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <pqxx/pqxx>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
+#include <QVector>
+#include <QString>
+
 namespace Dandelion::Server {
 
-struct Database {
-public:
+    struct Database {
+        std::mutex m_mutex;
+        pqxx::connection m_connection;
 
-private:
-    friend struct UserStorage;
-    friend struct DictionariesStorage;
-};
+        explicit Database(const std::string &connection_string);
 
-struct UserStorage {
-private:
-    Database &db;
-};
+        void do_query(const std::string &sql);
 
-struct DictionariesStorage {
-private:
-    Database &db;
-};
+    private:
+        friend struct User;
+        friend struct Dictionaries;
+    };
+
+    struct User {
+        std::string m_username;
+        std::string m_password;
+        User(std::string username, std::string password) : m_username(std::move(username)),
+                                                           m_password(std::move(password)) {};
+    };
+
+    struct User_Database {
+        static unsigned int is_used_login(const std::string &login, Database &db);
+        static User make_signing_in(const std::string &login, const std::string &password, Database &db);
+        static User create_user(const std::string &login, const std::string &password, Database &db);
+        static void create_personal_dictionary(pqxx::work &worker, const std::string &login);
+        static bool add_new_word(const std::string &login, const std::string &word, Database &db);
+        static QVector<QVector<QString>> get_vocabulary(const std::string &login, Database &db);
+    };
+
+    struct Dictionaries_Database {
+        //TODO
+    };
 
 
 }  // namespace Dandelion::Server
--- server/include/server.hpp
+++ server/include/server.hpp
@@ -1,12 +1,111 @@
-#ifndef UNTITLED2_SERVER_HPP
-#define UNTITLED2_SERVER_HPP
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QMessageBox>
+#include <QTcpServer>
+#include <QWidget>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include "database.hpp"
+#include <QMap>
+
+class QTcpServer;
+
+class QTcpSocket;
+
+struct client_socket;
+
 namespace Dandelion::Server {
-    struct server {
+    class client_socket;
+
+    class server : public QObject {
+    Q_OBJECT
+    private:
+        QByteArray Data;
+        qint16 PORT{};
+        Database *m_db{};
+        QMap<qint64, client_socket *> m_sockets;
+    public:
+        QTcpServer *m_Server{};
+
+        ~server() override;
+
+        bool start_server(qint16 port, std::string &config_file);
+
+        bool connect_to_database(std::string &config_file);
+
+        server(QObject *parent = nullptr);
+
+        static server &getInstance() {
+            static server m_Server;
+            return m_Server;
+        };
+
+        void disconnect_socket(client_socket *socket);
+
+    public slots:
+
+        void incomingConnection();
+    };
+
+
+//////////NEW
+
+
+    struct client_request;
+
+    class client_socket : public QObject {
+    Q_OBJECT
+
+    private:
+        Database *m_db;
+        server *m_server;
+        quint16 m_socket_id;
+        client_request *m_request;
+    public:
+        QTcpSocket *m_socket;
+
+        ~client_socket() override;
+
+        void send_to_client(const QByteArray &data);
+
+        explicit client_socket(QTcpSocket *socket, Database *db, server *server, quint16 id, QObject *parent = nullptr);
+
+        int get_socket_id();
+
+    public slots:
+
+        void read_from_client();
+
+        void disconnect();
 
     };
-    struct get_client{
 
+
+//////////NEW
+
+
+    class client_request : public QObject {
+    Q_OBJECT
+        Database *m_db;
+        client_socket *request_sender;
+    public:
+        explicit client_request(Database *db);
+
+        QJsonDocument check_request(User probably_new_user, const std::string &error_text);
+
+        QJsonDocument validate_request(const QByteArray &data);
+
+        QJsonDocument make_registration(const QJsonObject &request);
+
+        QJsonDocument sign_in(const QJsonObject &request);
+
+        QJsonDocument add_new_word(const QJsonObject &request);
+
+        QJsonDocument get_words_from_vocabulary(const QJsonObject &request);
     };
+
+
 }
 
-#endif //UNTITLED2_SERVER_HPP
+#endif // SERVER_H
